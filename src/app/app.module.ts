d92b3310a5d384aadcdde81cb7917547b17d2e3e
Arman Moztarzadeh <armanmoztar@outlook.com>
2023-06-18 21:46:26 -0700

    footer + styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-// import { HomeComponent } from './home.component';
-// import { AboutComponent } from './about.component';
-// import { ProductsComponent } from './products.component';
-// import { ContactComponent } from './contact.component';
+import { GettingStartedComponent } from './getting-started/getting-started.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    GettingStartedComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: 'getting-started', component: GettingStartedComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent }
+    ]),
     AppRoutingModule
   ],
   providers: [],
